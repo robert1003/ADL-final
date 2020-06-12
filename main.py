94f3ae78dc992cedbfbaa9a3835a676d066589c6
robert1003 <b07902047@ntu.edu.tw>
2020-06-12 08:53:31 +0800

    remove checking part

--- main.py
+++ main.py
@@ -65,41 +65,6 @@ def main():
     QAFeatures_dev, QAdataset_dev = QAExamples_to_QAFeatureDataset(QAExamples_dev, tokenizer, 'train')
     print('DONE', file=sys.stderr)
     
-    # data checking
-    print('Checking data...', file=sys.stderr)
-    for fea in tqdm(QAFeatures_train):
-        data = QAExamples_train[fea.example_id]
-        s = tokenizer.decode(fea.input_ids[fea.start_position + 15:fea.end_position + 15 + 1], skip_special_tokens=False).replace(' ', '').replace('#', '')
-        try:
-            assert data.answer_text.find(s) != -1 or (s == '[SEP]' and not data.answerable)
-        except:
-            if s.find('[UNK]') == -1:
-                print(fea.tokens[fea.start_position:fea.end_position + 1], file=sys.stderr)
-                print(s, file=sys.stderr)
-                print(len(data.answer_text), file=sys.stderr)
-                print(data, file=sys.stderr)
-                print('\n\n', file=sys.stderr)
-
-        if fea.start_position >= 497 or fea.end_position >= 497:
-            print('fffffffff', fea.tokens[fea.start_position + 15:fea.end_position + 15 + 1], sys.stderr)
-        
-    for fea in tqdm(QAFeatures_dev):
-        data = QAExamples_dev[fea.example_id]
-        s = tokenizer.decode(fea.input_ids[fea.start_position + 15:fea.end_position + 15 + 1], skip_special_tokens=False).replace(' ', '').replace('#', '')
-        try:
-            assert data.answer_text.find(s) != -1 or (s == '[SEP]' and not data.answerable)
-        except:
-            if s.find('[UNK]') == -1:
-                print(fea.tokens[fea.start_position:fea.end_position + 1], file=sys.stderr)
-                print(s, file=sys.stderr)
-                print(len(data.answer_text), file=sys.stderr)
-                print(data, file=sys.stderr)
-                print('\n\n', file=sys.stderr)
-
-        if fea.start_position >= 497 or fea.end_position >= 497:
-            print('fffffffff', fea.tokens[fea.start_position + 15:fea.end_position + 15 + 1], file=sys.stderr)
-    print('DONE', file=sys.stderr)
-    
     # train model
     if args.train != None:
         if args.use_sampler:
