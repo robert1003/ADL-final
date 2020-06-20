d17bcfe3abd8eb2761fca92c5d940e255bdb6eb7
robert1003 <b07902047@ntu.edu.tw>
2020-06-20 21:24:37 +0800

    add sh file

---
+++ run.sh
@@ -0,0 +1,20 @@
+
+if [ $# -lt 3 ]; then
+  echo -e "usage:\t bash run.sh [model_type] [cuda_num] [name] other_args(optional)"
+  exit
+fi
+
+array=("$@")
+
+model_type=$1
+cuda_num=$2
+model_name=$3.pth
+log_name=$3.log
+other_args=""
+
+for ((i=3; i<$#; i++))
+do
+  other_args="$other_args ${array[$i]}"
+done
+
+echo "python3 main.py --train $model_type --cuda $cuda_num --model_name $model_name --log_name $log_name $other_args"
